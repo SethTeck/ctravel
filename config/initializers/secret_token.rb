d9cb9f0d0db8052de9eb782df3fe21589a44f95f
Seth Allen <seth.a@me.com>
2015-08-28 22:37:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ctravel::Application.config.secret_key_base = 'f8cccd226672d6d3bf6dbcc30a3854ff82016238800f081598f7cb6f6f0eb8cb8d2a805c15f27a424fdcb580dcfd786145f4ac35bfb406f682f321a9ddc746a2'
